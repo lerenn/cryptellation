558ca9eb98e996349e83db552262f2a2848c6c25
Louis FRADIN <louis.fradin@gmail.com>
2023-01-14 19:29:15 +0100

    typo

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -91,7 +91,7 @@ const config = {
             items: [
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/digital-feather/cryptellation',
               },
             ],
           },
