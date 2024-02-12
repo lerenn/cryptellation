e3bdc1be52b8e3e868476efab73aef8478634ab4
Louis FRADIN <louis.fradin@gmail.com>
2024-02-12 18:58:17 +0100

    add otelco

--- tools/make/docker-compose.mk
+++ tools/make/docker-compose.mk
@@ -1,5 +1,14 @@
 DOCKER_COMPOSE_CMD := docker compose -f ./deployments/docker-compose.yaml 
 
-.PHONY: serve
-serve:
-	@$(DOCKER_COMPOSE_CMD) up
+.PHONY: down
+down: ## Stop the development environment
+	@$(DOCKER_COMPOSE_CMD) down
+
+.PHONY: up
+up: ## Start the development environment
+	@$(DOCKER_COMPOSE_CMD) up \
+		--attach backtests-api \
+		--attach candlesticks-api \
+		--attach exchanges-api \
+		--attach indicators-api \
+		--attach ticks-api
