5bc8b378fb4346129e6726f2ea80062794da3e08
Louis Fradin <lerenn@users.noreply.github.com>
2024-09-24 10:51:58 +0200

    ci(dagger): fix integration and end-to-end tests (#206)

--- tools/make/dagger.mk
+++ tools/make/dagger.mk
@@ -13,23 +13,26 @@ dagger/lint: ## Run all linters through Dagger
 dagger/check-generation: ## Run all checks for generated code through Dagger
 	@dagger call -m $(DAGGER_MODULE_PATH) check-generation --source-dir=$(PROJECT_ROOT_PATH) stdout
 
-.PHONY: dagger/unit-tests
-dagger/unit-tests: ## Run all unit tests through Dagger
+.PHONY: dagger/tests/unit
+dagger/tests/unit: ## Run all unit tests through Dagger
 	@dagger call -m $(DAGGER_MODULE_PATH) unit-tests --source-dir=$(PROJECT_ROOT_PATH) stdout
 
-.PHONY: dagger/integration-tests
-dagger/integration-tests: ## Run all integration tests through Dagger
+.PHONY: dagger/tests/integration
+dagger/tests/integration: ## Run all integration tests through Dagger
 	@dagger call -m $(DAGGER_MODULE_PATH) integration-tests \
 		--source-dir=$(PROJECT_ROOT_PATH) \
 		--secrets-file=file:$(PROJECT_ROOT_PATH)/.credentials.env \
 		stdout
 
-.PHONY: dagger/end-to-end-tests
-dagger/end-to-end-tests: ## Run all end-to-end tests through Dagger
+.PHONY: dagger/tests/end-to-end
+dagger/tests/end-to-end: ## Run all end-to-end tests through Dagger
 	@dagger call -m $(DAGGER_MODULE_PATH) end-to-end-tests \
 		--source-dir=$(PROJECT_ROOT_PATH) \
 		--secrets-file=file:$(PROJECT_ROOT_PATH)/.credentials.env \
 		stdout
+
+.PHONY: dagger/tests
+dagger/tests: dagger/tests/unit dagger/tests/integration dagger/tests/end-to-end ## Run all tests through Dagger
 
 .PHONY: dagger/create-release
 dagger/create-release: ## Create a release of the new version through Dagger
