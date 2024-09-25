812b42e930414e8b42ecd93167064b1adb9d3cce
Louis FRADIN <louis.fradin@gmail.com>
2024-09-25 16:30:09 +0200

    add order display

--- tools/displayer/main.py
+++ tools/displayer/main.py
@@ -6,18 +6,32 @@ import mplfinance as mpf
 class Displayer(object):
     def __init__(self, data):
         self.candlesticks = data["candlesticks"]
+        self.orders = data["orders"]
 
     def display(self):
         for exchange in self.candlesticks:
             for pair in self.candlesticks[exchange]:
-                self._display_candlesticks(self.candlesticks[exchange][pair])
+                cds = self._load_candlesticks(self.candlesticks[exchange][pair])
+                buy, sell = self._load_orders(self.orders)
+                cds['buy'] = buy['price']
+                cds['sell'] = sell['price']
+                mpf.plot(cds, type='candle', warn_too_much_data=100000, addplot=[
+                    mpf.make_addplot(cds['buy'], scatter=True, marker='^'),
+                    mpf.make_addplot(cds['sell'], scatter=True, marker='v')
+                ])
                 
-    def _display_candlesticks(self, candlesticks):
-        cds_dataframe = pd.DataFrame(candlesticks)
-        cds_dataframe['time'] = pd.to_datetime(cds_dataframe['time'])
-        cds_dataframe.set_index('time', inplace=True)
+    def _load_candlesticks(self, candlesticks):
+        df = pd.DataFrame(candlesticks)
+        df['time'] = pd.to_datetime(df['time'])
+        df.set_index('time', inplace=True)
+        return df
+    
+    def _load_orders(self, orders):
+        df = pd.DataFrame(orders)
+        df['time'] = pd.to_datetime(df['execution_time'])
+        df.set_index('time', inplace=True)
 
-        mpf.plot(cds_dataframe, type='candle', warn_too_much_data=100000)
+        return df[df['side'] == 'buy'], df[df['side'] == 'sell']
 
 if __name__ == '__main__':
     with open(sys.argv[1]) as f:
