0d4d1377d49d9ea58fc61fc9fc1857249b536334
Louis Fradin <lerenn@users.noreply.github.com>
2025-03-13 17:44:35 +0100

    release: v1.9.1 (#43)
    Co-authored-by: Cryptellation CI <louis.fradin+cryptellation-ci@gmail.com>

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -8,7 +8,7 @@ const (
 	// DefaultHash is the default hash if there is no hash provided.
 	DefaultHash = "dev"
 	// DefaultVersion is the default version if there is no hash provided.
-	DefaultVersion = "1.9.0"
+	DefaultVersion = "1.9.1"
 )
 
 var (
