45a5ef0cff1d7c06188c1be8973db51a3e5e3c77
Louis FRADIN <louis.fradin@gmail.com>
2025-03-14 12:14:57 +0100

    add to docker-compose

--- tools/mk/docker.mk
+++ tools/mk/docker.mk
@@ -1,10 +1,22 @@
 DOCKER_CMD          := docker
-DOCKER_COMPOSE_CMD  := $(DOCKER_CMD) compose -f ./deployments/docker-compose/docker-compose.yaml
+DOCKER_COMPOSE_CMD  := $(DOCKER_CMD) compose -f ./deployments/docker-compose/docker-compose.yaml -p cryptellation
 DOCKER_IMAGE        := lerenn/cryptellation
 DOCKER_CFG_PATH     := ./build/package
 
 GIT_COMMIT_SHA      := $(shell git rev-parse HEAD)
 GIT_LAST_BRANCH_TAG := $(shell git describe --abbrev=0 --tags)
+
+.PHONY: docker/all/down
+docker/all/down: ## Stop the entire docker environment
+	@$(DOCKER_COMPOSE_CMD) \
+		--profile worker --profile ui \
+		down --volumes --remove-orphans
+
+.PHONY: docker/all/up
+docker/all/up: ## Start a full docker environment
+	@$(DOCKER_COMPOSE_CMD) \
+		--profile worker --profile ui \
+		up -d
 
 .PHONY: docker/build
 docker/build: ## Build the docker image
@@ -16,10 +28,9 @@ docker/build: ## Build the docker image
 		.
 
 .PHONY: docker/clean
-docker/clean: docker/env/down ## Clean the docker environment
+docker/clean: docker/all/down ## Clean the docker environment
 	@$(DOCKER_CMD) rmi $(DOCKER_IMAGE):devel || true
 	@$(DOCKER_CMD) buildx rm cryptellation || true
-	@$(DOCKER_COMPOSE_CMD) --profile worker --profile migrator down --volumes --remove-orphans
 
 .PHONY: docker/env/down 
 docker/env/down: ## Stop the dependencies in local environment
@@ -28,7 +39,6 @@ docker/env/down: ## Stop the dependencies in local environment
 .PHONY: docker/env/up
 docker/env/up: ## Start the dependencies in local environment
 	@$(DOCKER_COMPOSE_CMD) up -d
-	@$(DOCKER_COMPOSE_CMD) run migrator
 
 .PHONY: docker/publish
 docker/publish: ## Publish the docker image
@@ -42,10 +52,18 @@ docker/publish: ## Publish the docker image
 		--tag $(DOCKER_IMAGE):latest \
 		.
 
+.PHONY: docker/ui/up
+docker/ui/up: ## Start a cryptellation UI in local environment
+	@$(DOCKER_COMPOSE_CMD) --profile ui up -d
+
+.PHONY: docker/ui/down
+docker/ui/down: ## Stop a cryptellation UI in local environment
+	@$(DOCKER_COMPOSE_CMD) --profile ui down
+
 .PHONY: docker/worker/down
 docker/worker/down: ## Start a cryptellation worker in local environment
 	@$(DOCKER_COMPOSE_CMD) --profile worker down
 
 .PHONY: docker/worker/up
-docker/worker/up: docker/env/up ## Start a cryptellation worker in local environment
+docker/worker/up: ## Start a cryptellation worker in local environment
 	@$(DOCKER_COMPOSE_CMD) --profile worker up -d
